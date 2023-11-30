fe621479a7e3704861abf601b042c07f16ff9f91
Joel Correa <jcorrear468@gmail.com>
2023-11-30 13:58:51 -0300

    remove plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import path from "path"
 
 import { UserConfig, defineConfig } from "vite"
-import vercel from "vite-plugin-vercel"
 import svgr from "vite-plugin-svgr"
 import react from "@vitejs/plugin-react-swc"
 import { compression } from "vite-plugin-compression2"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), vercel(), compression({ algorithm: "brotliCompress" })],
+  plugins: [react(), svgr(), compression({ algorithm: "brotliCompress" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
