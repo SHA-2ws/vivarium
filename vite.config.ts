93df744b862b78724d4f4c92c56a81173f60f48d
Joel Correa <jcorrear468@gmail.com>
2023-11-30 13:57:24 -0300

    change deps

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { compression } from "vite-plugin-compression2"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vercel(), svgr(), react(), compression({ algorithm: "brotliCompress" })],
+  plugins: [react(), svgr(), vercel(), compression({ algorithm: "brotliCompress" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
